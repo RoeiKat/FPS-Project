94ddbbe107ca2f789eb097f1dda650f5a570fdf9
RoeiKat <xroeirk@gmail.com>
2022-09-18 20:57:36 +0300

    More work added to enemies

--- FPS-Project/Assets/Scripts/EnemyAI.cs
+++ FPS-Project/Assets/Scripts/EnemyAI.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    NavMeshAgent navMeshAgent;
+    public NavMeshAgent navMeshAgent;
     public Transform target;
     [SerializeField] float turnSpeed;
     Animator animator;
@@ -14,13 +14,20 @@ public class EnemyAI : MonoBehaviour
     public float chaseRange = 10f;
     float distanceToTarget = Mathf.Infinity;
     public bool isProvoked = false;
+    public bool stopMovement = false;
+    public bool isAttacking = false;
     public bool startScream = false;
     public Transform hitCheck;
+
+    [Header("Sounds")]
+    public AudioSource sfx;
+    public AudioClip idleSound, attackSound, screamSound;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         navMeshAgent = GetComponent<NavMeshAgent>();
+        sfx.Play();
     }
 
     void Update()
@@ -50,11 +57,17 @@ public class EnemyAI : MonoBehaviour
         faceTarget();
         if(distanceToTarget >= navMeshAgent.stoppingDistance)
         {
-            chaseTarget();
+            if(!isAttacking && !stopMovement)
+            {
+             chaseTarget();
+            }
         }   
         if(distanceToTarget <= navMeshAgent.stoppingDistance)
         {
-            attackTarget();
+            if(!isAttacking)
+            {
+                StartCoroutine(attackTarget());
+            }
         }
         else
         {
@@ -77,15 +90,37 @@ public class EnemyAI : MonoBehaviour
     
     private void chaseTarget()
     {
+        if(sfx.clip != screamSound)
+        {
+            sfx.clip = screamSound;
+            sfx.Play();
+        }
         navMeshAgent.SetDestination(target.position);
         hitCheck.GetComponent<HitHandler>().enabled = false;
     }
     
-    private void attackTarget()
+    // private void attackTarget()
+    // {
+    //     Debug.Log("Attacking the player");
+    //     animator.SetBool("attack", true);
+    //     hitCheck.GetComponent<HitHandler>().enabled = true;
+    // }
+
+    IEnumerator attackTarget()
     {
-        Debug.Log("Attacking the player");
-        animator.SetBool("attack", true);
+        isAttacking = true;
+        stopMovement = true;
+        sfx.clip = attackSound;
+        sfx.Play();
+        animator.SetBool("attack",true);
         hitCheck.GetComponent<HitHandler>().enabled = true;
+        do
+        {
+            yield return new WaitForEndOfFrame();
+        } 
+        while (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f);
+        isAttacking = false;
+        stopMovement = false;
     }
 
     private void faceTarget()
@@ -96,14 +131,20 @@ public class EnemyAI : MonoBehaviour
     }
 
     IEnumerator zombieScream()
-    {
+    { 
+        startScream = false;
+        stopMovement = true;
+        Debug.Log("Screaming");
+        sfx.clip = screamSound;
+        sfx.Play();
         animator.SetBool("scream", true);
         do
         {
             yield return new WaitForEndOfFrame();
-        } while (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f);
+        } 
+        while (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f);
         animator.SetBool("scream", false);
-        startScream = false;
+        stopMovement = false;
         engageTarget();
     }
 
--- FPS-Project/Assets/Scripts/EnemyHealth.cs
+++ FPS-Project/Assets/Scripts/EnemyHealth.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     public float health = 100f;
+    public float destroyTimer = 10f;
+    public float crawlHealth = 20f;
+    private bool hasCrawled = false;
+    public bool isDead = false;    
     EnemyAI enemyAI;
+    Animator animator;
+
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
         enemyAI = GetComponent<EnemyAI>();
     }
 
@@ -18,11 +25,52 @@ public class EnemyHealth : MonoBehaviour
         enemyAI.damageTaken();
         if(health <= 0)
         {
-            Die();
+            die();
         }
     }
-    void Die()
+    void die()
     {
-        Destroy(gameObject);
+        isDead = true;
+        int randomDeath = Random.Range(2,2);
+        int randomCrawl = Random.Range(1,1);
+        if(isDead)
+        {
+            if(randomCrawl == 1 && randomDeath == 2 && !hasCrawled)
+            {
+                StartCoroutine(startCrawl());
+                return;
+            }
+        if(hasCrawled)
+        {
+            animator.StopPlayback();
+            enemyAI.enabled = false;
+            enemyAI.sfx.Stop();
+            Destroy(gameObject, destroyTimer);
+            return;
+        }
+        animator.Play("zombie_death" + randomDeath);
+        enemyAI.enabled = false;
+        enemyAI.sfx.Stop();
+        Destroy(gameObject, destroyTimer);
+        }
+    }
+    IEnumerator startCrawl()
+    {
+        enemyAI.enabled = true;
+        enemyAI.stopMovement = true;
+        hasCrawled = true;
+        isDead = false;
+        health  = crawlHealth;
+        animator.Play("zombie_death2");
+        do
+        {
+            yield return new WaitForEndOfFrame();
+        } 
+        while (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f);
+        enemyAI.navMeshAgent.speed = 1f;
+        animator.SetTrigger("crawl");
+        enemyAI.stopMovement = false;
+        enemyAI.sfx.clip = enemyAI.screamSound;
+        enemyAI.sfx.Play();
     }
 }
